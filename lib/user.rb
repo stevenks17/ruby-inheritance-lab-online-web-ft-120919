66955fc2cb194669eaa93c998e61091302a61c5f
Steven Kumarsingh <stevenks17@gmail.com>
2020-01-09 00:50:56 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,12 +2,12 @@ class User
   
   attr_accessor :first_name, :last_name
   
-  @@all = []
+
   
-  def intitialize(first_name, last_name)
+  def intitialize(first_name = nil, last_name = nil) 
     @first_name = first_name
     @last_name = last_name
-    @@all << self
+
   end
   
 end
